ff6e8f49dc98a8c1fc253d5b1de506195e6b8b23
melarozz <melarozz18@gmail.com>
2024-11-18 11:20:28 +0700

    add: initial

---
+++ newspaper-admin/src/app/advertisement-templates/page.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import {
+  DateField,
+  DeleteButton,
+  EditButton,
+  List,
+  MarkdownField,
+  ShowButton,
+  useTable,
+} from "@refinedev/antd";
+import { type BaseRecord } from "@refinedev/core";
+import { Space, Table } from "antd";
+
+export default function BlogPostList() {
+  const { tableProps, filters } = useTable<{
+    widthInColumns: number,
+    heightInColumns: number,
+    createdAt: Date,
+    updatedAt: Date,
+    id: number | string,
+  }[]>({
+    syncWithLocation: true,
+    sorters: {
+      initial: [
+        {
+          field: 'id',
+          order: 'desc',
+        },
+      ],
+    },
+  });
+
+  return (
+    <List>
+      <Table {...tableProps} rowKey="id">
+        <Table.Column dataIndex="id" title={"ID"} />
+        <Table.Column dataIndex="widthInColumns" title={"Width"} />
+        <Table.Column dataIndex="heightInColumns" title={"Height"} />
+        <Table.Column
+          title={"Actions"}
+          dataIndex="actions"
+          render={(_, record: BaseRecord) => (
+            <Space>
+              <EditButton hideText size="small" recordItemId={record.id} />
+              <ShowButton hideText size="small" recordItemId={record.id} />
+              <DeleteButton hideText size="small" recordItemId={record.id} />
+            </Space>
+          )}
+        />
+      </Table>
+    </List>
+  );
+}
