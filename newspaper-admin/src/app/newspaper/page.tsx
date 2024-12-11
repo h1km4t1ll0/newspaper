3e829e3ec2e6cd4a68af968dc8c3e44e1686fa41
h1km4t1ll0 <dolgovdaniil007@gmail.com>
2024-12-11 10:46:14 +0700

    fix: эдитор работает нормально

--- newspaper-admin/src/app/newspaper/page.tsx
+++ newspaper-admin/src/app/newspaper/page.tsx
@@ -1,9 +1,22 @@
 "use client";
 
 import ContentEditor from "@components/editor-js/ContentEditor";
+import {Col, Row} from "antd";
 
 export default function BlogPostList() {
   return (
-      <ContentEditor/>
+    <div style={{width: '100%'}}>
+      <Row gutter={16} style={{width: '100%'}}>
+        <Col md={8} xs={24}>
+          <ContentEditor/>
+        </Col>
+        <Col md={8} xs={24}>
+          <ContentEditor/>
+        </Col>
+        <Col md={8} xs={24}>
+          <ContentEditor/>
+        </Col>
+      </Row>
+    </div>
   );
 }
