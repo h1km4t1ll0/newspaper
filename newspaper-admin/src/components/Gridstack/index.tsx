d832a945e374aeaa6747b2d0cb5396c9dd2a1683
melarozz <melarozz18@gmail.com>
2024-12-15 22:29:23 +0700

    fix: editorjs inside blocks

--- newspaper-admin/src/components/Gridstack/index.tsx
+++ newspaper-admin/src/components/Gridstack/index.tsx
@@ -1,93 +1,34 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 import "./grid-stack.css";
 import { Grid } from "./Grid";
+import EditorJSComponent from "./EditorJSComponent";
 
-type layout = { id: string; x: number; y: number; w: number; h: number }[];
-type widget = { id: string; content: string; lock: boolean };
-
-/*
-[
-    {
-        "id": "1",
-        "content": "widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1",
-        "lock": false
-    },
-    {
-        "id": "2",
-        "content": "widget 2",
-        "lock": false
-    },
-    {
-        "id": "3",
-        "content": "widget 3",
-        "lock": false
-    },
-    {
-        "id": "4",
-        "content": "widget 4",
-        "lock": false
-    },
-    {
-        "id": "5",
-        "content": "widget 5",
-        "lock": false
-    },
-    {
-        "id": "6",
-        "content": "widget 6",
-        "lock": false
-    },
-    {
-        "id": "7",
-        "content": "widget 7",
-        "lock": false
-    },
-    {
-        "id": "8",
-        "content": "widget 8",
-        "lock": false
-    },
-    {
-        "id": "9",
-        "content": "widget 9",
-        "lock": false
-    },
-    {
-        "id": "10",
-        "content": "widget 10",
-        "lock": false
-    },
-    {
-        "id": "11",
-        "content": "widget 11",
-        "lock": false
-    }
-]
- */
+type Layout = { id: string; x: number; y: number; w: number; h: number }[];
+type Widget = { id: string; content: string; lock: boolean };
 
 const GridStack = () => {
   const [layout, setLayout] = useState<any>({
-    "1": {x: 0, y: 0, w: 1, h: 1},
-    "2": {x: 1, y: 0, w: 1, h: 1},
-    "3": {x: 2, y: 0, w: 1, h: 1},
-    "4": {x: 3, y: 0, w: 1, h: 1},
+    "1": { x: 0, y: 0, w: 1, h: 1 },
+    "2": { x: 1, y: 0, w: 1, h: 1 },
+    "3": { x: 2, y: 0, w: 1, h: 1 },
+    "4": { x: 3, y: 0, w: 1, h: 1 },
   });
 
-  const [widgets, setWidgets] = useState<widget[]>([
-    { id: "1", content: "widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1widget 1", lock: false },
+  const [widgets, setWidgets] = useState<Widget[]>([
+    { id: "1", content: "widget 1", lock: false },
     { id: "2", content: "widget 2", lock: false },
     { id: "3", content: "widget 3", lock: false },
     { id: "4", content: "widget 4", lock: false },
   ]);
 
   useEffect(() => {
-    console.log(layout, widgets)
+    console.log(layout, widgets);
   }, [widgets, layout]);
 
-  const updateLayoutHandle = (layout: layout) => console.log(layout);
+  const updateLayoutHandle = (layout: Layout) => console.log(layout);
 
   const addWidget = () => {
-    const newWidget: widget = {
+    const newWidget: Widget = {
       id: (widgets.length + 1).toString(),
       content: "widget " + (widgets.length + 1).toString(),
       lock: false,
@@ -102,21 +43,21 @@ const GridStack = () => {
 
   return (
       <Grid
-        layout={layout}
-        updateLayoutHandle={updateLayoutHandle}
-        addWidget={addWidget}
-        removeWidget={removeWidget}
-        onChangeLayout={setLayout}
+          layout={layout}
+          updateLayoutHandle={updateLayoutHandle}
+          addWidget={addWidget}
+          removeWidget={removeWidget}
+          onChangeLayout={setLayout}
       >
         {widgets.map((widget) => (
-          <div
-            className="widget"
-            key={widget.id}
-            id={widget.id}
-            data-lock={widget.lock}
-          >
-            {widget.content}
-          </div>
+            <div
+                className="widget"
+                key={widget.id}
+                id={widget.id}
+                data-lock={widget.lock}
+            >
+              <EditorJSComponent widgetId={widget.id} />
+            </div>
         ))}
       </Grid>
   );
